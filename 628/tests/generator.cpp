577e71d8f7a7e0d8b2aa4910d30f83f6c23e71cb
uPiscium <upiscium@gmail.com>
2025-01-15 16:21:52 +0900

    Add AC, WA, and validator for 628

--- 628/tests/generator.cpp
+++ 628/tests/generator.cpp
@@ -1,16 +1,16 @@
 #include "testlib.h"
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int MIN_A = 1;
-const int MAX_A = 1000;
+const int MAX_A = 100;
 const int MIN_B = 1;
-const int MAX_B = 1000;
+const int MAX_B = 100;
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
   registerGen(argc, argv, 1);
   for (int t = 0; t < 20; t++) {
-    ofstream of(format("02_random_%02d.in", t+1).c_str());
+    ofstream of(format("02_random_%02d.in", t + 1).c_str());
     of << rnd.next(MIN_A, MAX_A) << " " << rnd.next(MIN_B, MAX_B) << endl;
     of.close();
   }
