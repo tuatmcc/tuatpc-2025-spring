03e5a6a391963c8624726ec0e981345c4fc9e6c7
thisagi <106305548+thisagi@users.noreply.github.com>
2025-01-15 15:07:43 +0900

    add 628 by hisagi

---
+++ 628/tests/generator.cpp
@@ -0,0 +1,18 @@
+#include "testlib.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MIN_A = 1;
+const int MAX_A = 1000;
+const int MIN_B = 1;
+const int MAX_B = 1000;
+
+int main(int argc, char* argv[]){
+  registerGen(argc, argv, 1);
+  for (int t = 0; t < 20; t++) {
+    ofstream of(format("02_random_%02d.in", t+1).c_str());
+    of << rnd.next(MIN_A, MAX_A) << " " << rnd.next(MIN_B, MAX_B) << endl;
+    of.close();
+  }
+  return 0;
+}
