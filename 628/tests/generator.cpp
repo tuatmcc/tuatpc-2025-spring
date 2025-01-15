92da2a5bf9f0891889a6c5a7d88e7d2b7e7488b1
thisagi <106305548+thisagi@users.noreply.github.com>
2025-01-15 15:50:36 +0900

    add sample input

--- 628/tests/generator.cpp
+++ 628/tests/generator.cpp
@@ -7,12 +7,28 @@ const int MAX_A = 1000;
 const int MIN_B = 1;
 const int MAX_B = 1000;
 
+void gen(string name, int A, int B){
+    ofstream ofs(name);
+    ofs << A << " " << B << endl;
+}
+
 int main(int argc, char* argv[]){
   registerGen(argc, argv, 1);
+
+  // sample1
+  gen("00_sample_01.in", 5, 2);
+
+  // sample2
+  gen("00_sample_02.in", 1, 45);
+
+  // min
+  gen("min.in", 1, 1);
+
+  // max
+  gen("max.in", 100, 100);
+
   for (int t = 0; t < 20; t++) {
-    ofstream of(format("02_random_%02d.in", t+1).c_str());
-    of << rnd.next(MIN_A, MAX_A) << " " << rnd.next(MIN_B, MAX_B) << endl;
-    of.close();
+    gen(format("02_random_%02d.in", t+1).c_str(), rnd.next(MIN_A, MAX_A), rnd.next(MIN_B, MAX_B));
   }
   return 0;
 }
