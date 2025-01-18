7877f5dae5d443a14a2d9cac6e75ac7dacb5f102
toufu24 <120612884+toufu-24@users.noreply.github.com>
2025-01-18 23:28:09 +0900

    Fix constrains.h

--- amidakuji/tests/constrains.h
+++ amidakuji/tests/constrains.h
@@ -2,11 +2,11 @@
 #define CONSTRAINS_H
 
 // 定数定義
-constexpr int MIN_W = 2;
-constexpr int MAX_W = 60;
-constexpr int MIN_H = 1;
-constexpr int MAX_H = 100000;
-constexpr int MIN_Q = 1;
-constexpr int MAX_Q = 100000;
+const int MIN_W = 2;
+const int MAX_W = 60;
+const int MIN_H = 1;
+const int MAX_H = 100000;
+const int MIN_Q = 1;
+const int MAX_Q = 100000;
 
 #endif
