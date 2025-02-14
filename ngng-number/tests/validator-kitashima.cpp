930ab3fb4861fdcaddb135f174be0daf376001c8
kitashima_desktop <110659558+kitashimauni@users.noreply.github.com>
2025-02-15 03:12:18 +0900

    validatorの修正

--- ngng-number/tests/validator-kitashima.cpp
+++ ngng-number/tests/validator-kitashima.cpp
@@ -9,8 +9,8 @@ int main() {
   registerValidation();
   
   // 1以上10^100000以下の整数を読み込む
-  string s = inf.readToken(format("([1-9][0-9]{0,%d}|10{%d})", MAX_DIGIT - 1, MAX_DIGIT));
-
+  string s = inf.readToken(format("([1-9][0-9]{0,%d}|10{%d})", MAX_length_of_N - 1, MAX_length_of_N));
+  inf.readEoln();
   inf.readEof();
   return 0;
 }
