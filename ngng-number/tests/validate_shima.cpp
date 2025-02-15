ff881f96d07aac0afe099d101ece4cb6cccf67d0
shim4stat <s234510v@st.go.tuat.ac.jp>
2025-02-15 18:42:41 +0900

    added constraints.h

--- ngng-number/tests/validate_shima.cpp
+++ ngng-number/tests/validate_shima.cpp
@@ -1,17 +1,17 @@
+#include "constraints.h"
 #include "testlib.h"
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc, char* argv[]){
-  registerValidation(argc, argv);
-  // regx no shiyou wakaran
-  string num =  inf.readToken("[0-9]{0, 100001}");
-  ensure(num[0] != '0');
-  if(num.size == 100001){
-      ensure(num[0] == '1');
-      for(int i = 1; i < num.size(); i++){
-          ensure(num[i] == '0');
-      }
-  }
-  inf.readEof();
+int main(int argc, char *argv[]) {
+    registerValidation(argc, argv);
+    string num = inf.readToken(format("[0-9]{%d, %d}", MIN_length_of_N, MAX_length_of_N + 1));
+    ensure(num[0] != '0');
+    if (num.size == 100001) {
+        ensure(num[0] == '1');
+        for (int i = 1; i < num.size(); i++) {
+            ensure(num[i] == '0');
+        }
+    }
+    inf.readEof();
 }
