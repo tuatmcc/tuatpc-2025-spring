7d7a96689b8e9cfc217e3f9d52ac83b1c48146ff
shim4stat <s234510v@st.go.tuat.ac.jp>
2025-02-17 11:51:05 +0900

    fixed validator

--- ngng-number/tests/validate_shima.cpp
+++ ngng-number/tests/validate_shima.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     registerValidation(argc, argv);
-    string num = inf.readToken(format("[0-9]{%d, %d}", MIN_length_of_N, MAX_length_of_N + 1));
+    string num = inf.readToken(std::format("[0-9]{{{}, {}}}", MIN_length_of_N, MAX_length_of_N + 1));
     ensure(num[0] != '0');
-    if (num.size == 100001) {
+    if (num.size() == MAX_length_of_N + 1) {
         ensure(num[0] == '1');
         for (int i = 1; i < num.size(); i++) {
             ensure(num[i] == '0');
