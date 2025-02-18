8a48aebb3c77acd6740f1365e593835ea132d32a
sunrize0714 <mkm@DESKTOP-CVJHATM.localdomain>
2025-02-19 01:35:21 +0900

    テストケースを複数に

--- ngng-number/tests/generator.cpp
+++ ngng-number/tests/generator.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     registerGen(argc,argv,1);
-
-    ofstream of(format("001_small_rid_%02d.in").c_str());
-    const int N = rnd.next(MIN_length_of_N,MAX_length_of_N);
-    string S(N,'.');
-    for(int i=0;i<N;i++){
-        if(i == 0)S[i] = (char)('0' + rnd.next(1,9));
-        else S[i] = (char)('0' + rnd.next(0,9));
+    for(int i=0;i<20;i++){
+        ofstream of(format("001_small_rid_%02d.in",i+1).c_str());
+        const int N = rnd.next(MIN_length_of_N,MAX_length_of_N);
+        string S(N,'.');
+        for(int i=0;i<N;i++){
+            if(i == 0)S[i] = (char)('0' + rnd.next(1,9));
+            else S[i] = (char)('0' + rnd.next(0,9));
+        }
+        of << S << endl;
     }
-    of << S << endl;
 }
