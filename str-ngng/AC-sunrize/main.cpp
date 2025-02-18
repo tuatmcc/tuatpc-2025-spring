51d928d09130af0c0f652d89b7caa684fcd95f95
sunrize0714 <mkm@DESKTOP-CVJHATM.localdomain>
2025-02-19 05:02:24 +0900

    作った

---
+++ str-ngng/AC-sunrize/main.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<vvvll> vvvvll;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll,ll>> vp;
+#define rep(i,a,n) for(ll i=a;i<n;i++)
+#define revrep(i,a,n) for(ll i=n-1;i>=a;i--)
+#define mod 998244353
+#define all(a) a.begin(),a.end()
+
+int main(){
+    ll n;
+    string s;
+    cin >> n >> s;
+    ll q;
+    cin >> q;
+    vll l(q),r(q);
+    rep(i,0,q)cin >> l[i] >> r[i];
+
+    
+
+} 
