9f8cc462d6110d56a187fadfc2d4d759bfdc5164
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-02-18 22:05:32 +0900

    不要な行を削除

--- amidakuji/AC-kobaryo222/main.cpp
+++ amidakuji/AC-kobaryo222/main.cpp
@@ -4,15 +4,10 @@ using namespace std;
 
 using ll = long long;
 
-template <class T> bool chmin(T &a, const T &b) { return a > b ? a = b, true : false; }
-template <class T> bool chmax(T &a, const T &b) { return a < b ? a = b, true : false; }
-
 struct IoSetup {
     IoSetup() {
         cin.tie(nullptr);
         ios::sync_with_stdio(false);
-        cout << fixed << setprecision(10);
-        cerr << fixed << setprecision(10);
     }
 } iosetup;
 
@@ -39,6 +34,7 @@ int main(){
             queries.emplace_back(t, s);
         }
     }
+    // y 座標について座圧する
     vector<ll> ys;
     for(auto q : queries){
         if(q.t != 3){
@@ -59,52 +55,16 @@ int main(){
     M = ys.size();
     ll block_size = sqrt(M) + 1;
     ll block_cnt = (M + block_size - 1) / block_size + 1;
-    vector<map<ll, ll>> to(block_cnt);
-    vector<map<ll, set<ll>>> lines(block_cnt);
-    // auto traverse = [&](ll x, ll y){
-    //     ll b = y / block_size;
-    //     ll nx = x, ny = y % block_size;
-    //     while(ny < block_size){
-    //         ll lidx, ridx;
-    //         {
-    //             ll ng = -1, ok = lines[b][nx - 1].size();
-    //             while(abs(ok - ng) > 1){
-    //                 ll mid = abs(ok + ng) / 2;
-    //                 if(lines[b][nx - 1][mid] >= ny) ok = mid;
-    //                 else ng = mid;
-    //             }
-    //             lidx = ok;
-    //         }
-    //         {
-    //             ll ok = -1, ng = lines[b][nx].size();
-    //             while(abs(ok - ng) > 1){
-    //                 ll mid = abs(ok + ng) / 2;
-    //                 if(lines[b][nx][mid] >= ny) ok = mid;
-    //                 else ng = mid;
-    //             }
-    //             ridx = ok;
-    //         }
-    //         ll l = block_size, r = block_size;
-    //         if(lidx != lines[b][nx - 1].size()) l = lines[b][nx - 1][lidx];
-    //         if(ridx != lines[b][nx].size()) r = lines[b][nx][ridx];
-    //         if(l <= r){
-    //             if(l == block_size) break;
-    //             ny = l;
-    //             nx--;
-    //         }else{
-    //             ny = r;
-    //             nx++;
-    //         }
-    //         ny++;
-    //     }
-    //     return nx;
-    // };
+    vector<map<ll, ll>> to(block_cnt); // to[i][j] := i 番目のブロック内で j 番目の縦線の先頭がどこに行くか
+    vector<map<ll, set<ll>>> lines(block_cnt); // lines[i][j] := i 番目のブロック内で j 番目の縦線から出ている横線の y 座標の集合
+    // x 番目の縦線の座標 y の位置からブロックの先頭まで逆順に辿ったときの縦線の番号を返す
     auto traverse_rev = [&](ll x, ll y){
-        ll b = y / block_size;
+        ll b = y / block_size; // block id
         ll nx = x, ny = y % block_size;
         while(ny >= 0){
-            ll l = -1, r = -1;
-            // assert(b < block_cnt);
+            // 次に移動する縦線が nx - 1 か nx + 1 かを判定する
+            // 現在の高さ ny より上にある左側の横線と右側の横線のうち、最も近いものを探す
+            ll l = -1, r = -1; //
             if(lines[b].contains(nx - 1)){
                 auto itrl = lines[b][nx - 1].upper_bound(ny);
                 if(itrl != lines[b][nx - 1].begin()) l = *prev(itrl);
@@ -113,6 +73,7 @@ int main(){
                 auto itr = lines[b][nx].upper_bound(ny);
                 if(itr != lines[b][nx].begin()) r = *prev(itr);
             }
+            // 近い方に移動する
             if(l >= r){
                 if(l == -1) break;
                 ny = l;
@@ -126,28 +87,25 @@ int main(){
         return nx;
     };
     auto query1 = [&](ll x, ll y){
+        // t = 1, 2 のクエリでは to[block_id] と line[block_id][x] を適切に変更する必要がある
+        // (x, y), (x + 1, y) から逆順にブロックの先頭まで辿ったときの縦線の番号を求める(それぞれ parl, parr とする)
+        // 横線を追加 / 削除したときは to[block_id][parl] と to[block_id][parr] を swap すればよい
         ll block_id = y / block_size;
         ll parl = traverse_rev(x, y);
         ll parr = traverse_rev(x + 1, y);
-        // cout << parl << ' ' << parr << '\n';
         if(!to[block_id].contains(parl)) to[block_id][parl] = parl;
         if(!to[block_id].contains(parr)) to[block_id][parr] = parr;
         swap(to[block_id][parl], to[block_id][parr]);
         lines[block_id][x].emplace(y % block_size);
-        // assert(traverse_rev(x, y) == parr);
-        // assert(traverse_rev(x + 1, y) == parl);
     };
     auto query2 = [&](ll x, ll y){
         ll block_id = y / block_size;
         ll parl = traverse_rev(x, y);
         ll parr = traverse_rev(x + 1, y);
-        // cout << parl << ' ' << parr << '\n';
         if(!to[block_id].contains(parl)) to[block_id][parl] = parl;
         if(!to[block_id].contains(parr)) to[block_id][parr] = parr;
         swap(to[block_id][parl], to[block_id][parr]);
         lines[block_id][x].erase(y % block_size);
-        // assert(traverse_rev(x, y) == parr);
-        // assert(traverse_rev(x + 1, y) == parl);
     };
     auto query3 = [&](ll s){
         ll cur = s;
@@ -159,7 +117,6 @@ int main(){
         return cur;
     };
     for(auto q : queries){
-        // cout << "query: " << q.t << ' ' << q.x << ' ' << q.y << ' ' << q.s << '\n';
         if(q.t == 1){
             query1(q.x, q.y);
         }else if(q.t == 2){
@@ -167,13 +124,5 @@ int main(){
         }else{
             cout << query3(q.s) << '\n';
         }
-        // for(int i = 1; i <= N; i++){
-        //     cout << i << ' ';
-        // }
-        // cout << '\n';
-        // for(int i = 1; i <= N; i++){
-        //     cout << query3(i) << ' ';
-        // }
-        // cout << '\n';
     }
 }
