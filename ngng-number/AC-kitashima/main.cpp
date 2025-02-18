6703b3ba88b98860c9b2a033555b4647cf11c403
kitashima_desktop <110659558+kitashimauni@users.noreply.github.com>
2025-02-18 20:56:55 +0900

    コードの誤りを修正

--- ngng-number/AC-kitashima/main.cpp
+++ ngng-number/AC-kitashima/main.cpp
@@ -9,10 +9,10 @@ int main() {
     string S;
     cin >> S;
 
-    // n_index: 上位n_index桁がn..
-    // ng_index: 上位ng_index桁がn..g..
+    // n_index: 上位n_index桁がn'..
+    // ng_index: 上位ng_index桁がn'..g'..
     int n_index = 0, ng_index = 0;
-    char n = S[0], g;
+    char n = S[0], g;  // n'とg'の値
     int i = 0;
     while (S[i] == n) i++;
     n_index = i;
@@ -33,8 +33,8 @@ int main() {
         // 一致している場合の処理
         if (i < n_index) {  // 上位n_index桁がn..
             dp[i][0][0] = 1;
-        } else if (i == n_index) {  // n数: {n..}にnがつく
-            if (S[i] > n) {         // S[i]がnより大きくS未満
+        } else if (i == n_index) {  // n数: {n'..}にn'がつく
+            if (S[i] > n) {         // S[i]がn'より大きい(S未満)
                 dp[i][1][0]++;
             }
         }
@@ -44,28 +44,29 @@ int main() {
 
         // ng数について
         // 一致している場合の処理
-        if (i < n_index) {  // n数: {n..}にg(n未満)がつく
-            dp[i][1][1] += (n - '0') - 1;
-        } else if (n_index <= i && i < ng_index) {  // ng数: n..g..
+        if (i < n_index) {  // n数: {'n..}にg(n'未満)がつく
+            dp[i][1][1] += (n - '0');
+        } else if (n_index <= i && i < ng_index) {  // ng数: n'..g'..
             dp[i][0][1] = 1;
-        } else if (i == ng_index && g < S[i]) {  // 夢は潰えない…
+        } else if (i == ng_index && S[i] > g) {  // ng数: {n'..g'..}にg'がつく
             dp[i][1][1]++;
         }
-        if (i == n_index) {  // n数: {n..}にg(g自身とnを除く)がつく
+        if (i == n_index) {  // n数: {n'..}にg(g'自身とn'を除く)がつく
             dp[i][1][1] += (g - '0') - (n < g ? 1 : 0);
         }
         // 未満フラグに関する処理
-        dp[i][1][1] += dp[i - 1][1][0] * 8;  // n数: {n..}にgがつく
+        dp[i][1][1] += dp[i - 1][1][1];      // ng数: {n..g..}にgがつく
+        dp[i][1][1] += dp[i - 1][1][0] * 9;  // n数: {n..}にgがつく
 
         // ngn数について
         // 一致している場合の処理
-        if (i == ng_index ||  // ng数: {n..g..}にnがつく または
+        if (i == ng_index ||  // ng数: {n'..g'..}にn'がつく または
             (i > ng_index &&
-             (dp[i - 1][0][2] == 1 ||    // ngn数: {n..g..n..}にnがつく または
-              dp[i - 1][0][3] == 1))) {  // ngng数: {n..g..n..g..}にnがつく
-            if (S[i] == n) {             // S[i]がnでSと一致
+             (dp[i - 1][0][2] == 1 ||  // ngn数: {n'..g'..n'..}にn'がつく または
+              dp[i - 1][0][3] == 1))) {  // ngng数: {n'..g'..n'..g'..}にnがつく
+            if (S[i] == n) {             // S[i]がn'でSと一致
                 dp[i][0][2] = 1;
-            } else if (S[i] > n) {  // S[i]がnより大きくS未満
+            } else if (S[i] > n) {  // S[i]がn'より大きい(S未満)
                 dp[i][1][2]++;
             }
         }
@@ -76,11 +77,11 @@ int main() {
 
         // ngng数について
         // 一致している場合の処理
-        if (dp[i - 1][0][2] == 1 ||  // ngn数: {n..g..n..}にgがつく または
-            dp[i - 1][0][3] == 1) {  // ngng数: {n..g..n..g..}にgがつく
-            if (S[i] == g) {         // S[i]がgでSと一致
+        if (dp[i - 1][0][2] == 1 ||  // ngn数: {n'..g'..n'..}にg'がつく または
+            dp[i - 1][0][3] == 1) {  // ngng数: {n'..g'..n'..g'..}にg'がつく
+            if (S[i] == g) {         // S[i]がg'でSと一致
                 dp[i][0][3] = 1;
-            } else if (S[i] > g) {  // S[i]がgより大きくS未満
+            } else if (S[i] > g) {  // S[i]がg'より大きい(S未満)
                 dp[i][1][3]++;
             }
         }
