5e22b71492bf6f9a953da2a3aca5624f6e48c9cf
sunrize0714 <mkm@DESKTOP-CVJHATM.localdomain>
2025-02-19 23:43:59 +0900

    validator書いた

--- str-ngng/tests/validator-sunrize.cpp
+++ str-ngng/tests/validator-sunrize.cpp
@@ -10,7 +10,7 @@ int main(){
     inf.readSpace();
     int Q = inf.readInt(MIN_Q,MAX_Q,"Q");
     inf.readEoln();
-    string S = inf.readToken(format("[a-z]{%d}",N));
+    string S = inf.readString(format("[a-z]{%d}",N),"S");
     for(int i=0;i<Q;i++){
         int L = inf.readInt(1,N,"L");
         inf.readSpace();
