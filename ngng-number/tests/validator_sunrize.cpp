7a199b533d30ff4ecdc03f0e9707e8eb7204111e
sunrize0714 <mkm@DESKTOP-CVJHATM.localdomain>
2025-02-19 15:03:50 +0900

    Validator直した

--- ngng-number/tests/validator_sunrize.cpp
+++ ngng-number/tests/validator_sunrize.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-    registerInteraction(argc,argv);
+int main(){
+    registerInteraction();
     string S = inf.readToken(format("([1-9][0-9]{0,%d}|10{%d})",MAX_length_of_N-1,MAX_length_of_N));
     inf.readEoln();
     inf.readEof();
