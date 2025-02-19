9d73bdd912c28dce3a6132127b8b57ff7d4cbb8e
sunrize0714 <mkm@DESKTOP-CVJHATM.localdomain>
2025-02-19 14:11:22 +0900

    テストケース改修

--- ngng-number/tests/generator.cpp
+++ ngng-number/tests/generator.cpp
@@ -16,71 +16,71 @@ int main(int argc, char* argv[]){
 
     // sample2
     string sample2 = "25500";
-    ofstream of(format("000_sample_%02d.in",2).c_str());
-    of << sample2 << endl;
-    of.close();
+    ofstream of2(format("000_sample_%02d.in",2).c_str());
+    of2 << sample2 << endl;
+    of2.close();
 
     // sample2
     string sample3 = "20250309";
-    ofstream of(format("000_sample_%02d.in",3).c_str());
-    of << sample3 << endl;
-    of.close();
+    ofstream of3(format("000_sample_%02d.in",3).c_str());
+    of3 << sample3 << endl;
+    of3.close();
 
     for(int i=0;i<10;i++){
-        ofstream of(format("001_subtask_%02d.in",i+1).c_str());
+        ofstream of4(format("001_subtask_%02d.in",i+1).c_str());
         const int N = rnd.next(MIN_length_of_N,MAX_length_of_N_subtask);
         string S(N,'.');
         for(int j=0;j<N;j++){
             if(j == 0)S[j] = (char)('0' + rnd.next(1,9));
             else S[j] = (char)('0' + rnd.next(0,9));
         }
-        of << S << endl;
-        of.close();
+        of4 << S << endl;
+        of4.close();
     }
 
     for(int i=0;i<20;i++){
-        ofstream of(format("002_random_rid_%02d.in",i+1).c_str());
+        ofstream of5(format("002_random_rid_%02d.in",i+1).c_str());
         const int N = rnd.next(MIN_length_of_N,MAX_length_of_N);
         string S(N,'.');
         for(int j=0;j<N;j++){
             if(j == 0)S[j] = (char)('0' + rnd.next(1,9));
             else S[j] = (char)('0' + rnd.next(0,9));
         }
-        of << S << endl;
-        of.close();
+        of5 << S << endl;
+        of5.close();
     }
 
 
     for(int i=0;i<5;i++){
-        ofstream of(format("003_long_%02d.in",i+1).c_str());
+        ofstream of6(format("003_long_%02d.in",i+1).c_str());
         const int N = rnd.next(100000,MAX_length_of_N);
         string S(N,'.');
         for(int j=0;j<N;j++){
             if(j == 0)S[j] = (char)('0' + rnd.next(1,9));
             else S[j] = (char)('0' + rnd.next(0,9));
         }
-        of << S << endl;
-        of.close();
+        of6 << S << endl;
+        of6.close();
     }
 
     // max-1
-    ofstream of(format("004_max-1_%02d.in",1).c_str());
+    ofstream of7(format("004_max-1_%02d.in",1).c_str());
     string S_max_minus_one(999999,'.');
     for(int j=0;j<999999;j++){
         if(j == 0)S_max_minus_one[j] = (char)('0' + rnd.next(1,9));
         else S_max_minus_one[j] = (char)('0' + rnd.next(0,9));
     }
-    of << S_max_minus_one << endl;
-    of.close();
+    of7 << S_max_minus_one << endl;
+    of7.close();
 
     // max
-    ofstream of(format("005_max_%02d.in",1).c_str());
+    ofstream of8(format("005_max_%02d.in",1).c_str());
     string S_max(1000000,'.');
     for(int j=0;j<1000000;j++){
         if(j == 0)S_max[j] = (char)('0' + rnd.next(1,9));
         else S_max[j] = (char)('0' + rnd.next(0,9));
     }
-    of << S_max << endl;
-    of.close();
+    of8 << S_max << endl;
+    of8.close();
 
 }
