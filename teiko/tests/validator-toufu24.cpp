04b86207d35956fe1fd707b540cd7832a940c1f3
toufu-24 <120612884+toufu-24@users.noreply.github.com>
2025-02-21 16:01:01 +0900

    validator強化

--- teiko/tests/validator-toufu24.cpp
+++ teiko/tests/validator-toufu24.cpp
@@ -17,8 +17,8 @@ int main(int argc, char *argv[]) {
     int W = 0;
     for (int i = 0; i < H; i++) {
         W = max(W, (int)s[i].size());
-        cerr << i << "  ";
-        cerr << s[i] << endl;
+        // cerr << i << "  ";
+        // cerr << s[i] << endl;
     }
 
     inf.ensuref(H >= 1 && W >= 1, "H and W must be >= 1\n");
@@ -77,7 +77,8 @@ int main(int argc, char *argv[]) {
                 for (int k = 0; k < 4; k++) {
                     int ni = i + dx[k];
                     int nj = j + dy[k];
-                    if (ni < 0 || ni >= H || nj < 0 || nj >= W) continue;
+                    if (ni < 0 || ni >= H || nj < 0 || nj >= W)
+                        continue;
                     if (k < 2) {
                         if (s[ni][nj] == '-') {
                             yoko = true;
@@ -90,8 +91,95 @@ int main(int argc, char *argv[]) {
                 }
                 assert(s[i][j] == '+');
                 // cerr << s[i][j] << " " << tate << " " << yoko << endl;
-                cerr << i << " " << j << " " << tate << " " << yoko << endl;
+                // cerr << i << " " << j << " " << tate << " " << yoko << endl;
                 inf.ensuref(tate && yoko, "invalid '+' at s[%d][%d]\n", i, j);
+            }
+        }
+    }
+
+    // o はi = 0, j = 0,W-1にある
+    inf.ensuref(s[0][0] == 'o', "s[0][0] must be 'o'\n");
+    inf.ensuref(s[0][W - 1] == 'o', "s[0][W-1] must be 'o'\n");
+
+    // 奇数行は|と のみ
+    // 偶数行は-と^とvとoと+と|と のみ
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < s[i].size(); j++) {
+            if (i % 2 == 0) {
+                set<char> st = {'-', '^', 'v', 'o', '+', ' ', '|'};
+                inf.ensuref(st.contains(s[i][j]), "Invalid character in even row at s[%d][%d]\n", i, j);
+            } else {
+                inf.ensuref(s[i][j] == '|' || s[i][j] == ' ', "Invalid character in odd row at s[%d][%d]\n", i, j);
+            }
+        }
+    }
+
+    // 回路は連結している
+    auto s2 = s;
+    queue<pair<int, int>> que;
+    que.push({0, 0});
+    s2[0][0] = ' ';
+    while (!que.empty()) {
+        auto [i, j] = que.front();
+        que.pop();
+        int dx[] = {0, 0, -1, 1};
+        int dy[] = {-1, 1, 0, 0};
+        for (int k = 0; k < 4; k++) {
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+            if (ni < 0 || ni >= H || nj < 0 || nj >= W)
+                continue;
+            if (s2[ni][nj] == ' ')
+                continue;
+            if (s2[ni][nj] == '|' || s2[ni][nj] == '-' || s2[ni][nj] == 'o' || s2[ni][nj] == '+' || s2[ni][nj] == '^' || s2[ni][nj] == 'v') {
+                s2[ni][nj] = ' ';
+                que.push({ni, nj});
+            }
+        }
+    }
+    // s2には空白のみが入っている
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < s[i].size(); j++) {
+            inf.ensuref(s2[i][j] == ' ', "Circuit is not connected\n");
+        }
+    }
+
+    // v,^は抵抗器のとこにしかない
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < s[i].size(); j++) {
+            int dx[] = {0, 0};
+            int dy[] = {-1, 1};
+            if (s[i][j] == '^' || s[i][j] == 'v') {
+                bool ok = false;
+                // 左右のどっちかに^vがある
+                for (int k = 0; k < 2; k++) {
+                    int ni = i + dx[k];
+                    int nj = j + dy[k];
+                    if (ni < 0 || ni >= H || nj < 0 || nj >= W)
+                        continue;
+                    if (s[ni][nj] == '^' || s[ni][nj] == 'v') {
+                        ok = true;
+                    }
+                }
+                inf.ensuref(ok, "There must be '^' or 'v' at s[%d][%d]\n", i, j);
+            }
+        }
+    }
+
+    // -があったら3つ以上連続している
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < s[i].size(); j++) {
+            if (s[i][j] == '-') {
+                int cnt = 1;
+                for (int k = j + 1; k < s[i].size(); k++) {
+                    if (s[i][k] == '-') {
+                        cnt++;
+                        j++;
+                    } else {
+                        break;
+                    }
+                }
+                inf.ensuref(cnt >= 3, "There must be at least 3 '-' at s[%d][%d]\n", i, j);
             }
         }
     }
