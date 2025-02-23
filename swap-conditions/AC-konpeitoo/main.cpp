918584df8a71ee770e2490b6ecb5fc0096f1a8db
konpeitoo11 <169228335+konpeitoo11@users.noreply.github.com>
2025-02-23 22:10:47 +0900

    修正

--- swap-conditions/AC-konpeitoo/main.cpp
+++ swap-conditions/AC-konpeitoo/main.cpp
@@ -2,68 +2,65 @@
 using namespace std;
 
 int main(){
-    int K;
-    cin >> K;
-    K--;
-    vector<long long> A(4);
-    for(int i = 0; i < 4; i++)cin >> A[i];
-    vector<int> J(3);
-    for(int i = 0; i < 3; i++){
-        cin >> J[i];
-        J[i]--;
-    }
-    //long long _min = 0, _max = 0;
-    //_max = min(A[J[0]], min(A[J[1]], A[J[2]] / 2));
-
-    //今のrankを求める。
-    int up = 0, down = 0;
-    for(int i = 0; i < 4; i++){
-        if(i == K)continue;
-        if(A[K] < A[i])up++;
-        else if(A[i] < A[K])down++;
-        else if(i < K)up++;
-        else down++;
-    }
-    int iniRank = up + 1;
-
-    //solve
-    vector<long long> a(4);
-    for(int i = 1; i < iniRank; i++){
-        //long long __min = _min, __max = _max, mid, ans = -1;
-        long long __min = 0, __max = 2000000000000000000, mid, ans = -1;
-        while(__min <= __max){
-            mid = (__min + __max) / 2;
-            for(int j = 0; j < 4; j++)a[j] = A[j];
-            //K
-            a[K] += 4 * mid;
-            a[J[0]] -= mid;
-            a[J[1]] -= mid;
-            a[J[2]] -= 2 * mid;
-            //rank
-            up = 0;
-            for(int j = 0; j < 4; j++){
-                if(j == K)continue;
-                if(a[K] < a[j])up++;
-                else if(a[j] < a[K]);
-                else if(j < K)up++;
-                else;
-            }
-            up++;
-
-            if(up == i){
-                ans = mid;
-                __max = mid - 1;
-            }else if(up < i){
-                __max = mid - 1;
-            }else{
-                __min = mid + 1;
-            }
-            
+    int T;
+    cin >> T;
+    for(int t = 0; t < T; t++){
+        int X;
+        cin >> X;
+        X--;
+        int Y;
+        cin >> Y;
+        Y--;
+        int R;
+        cin >> R;
+        vector<long long> A(4);
+        for(int i = 0; i < 4; i++)cin >> A[i];
+    
+        //今のrankを求める。
+        int up = 0, down = 0;
+        for(int i = 0; i < 4; i++){
+            if(i == X)continue;
+            if(A[X] < A[i])up++;
+            else if(A[i] < A[X])down++;
+            else if(i < X)up++;
+            else down++;
         }
+        int iniRank = up + 1;
+    
+        //solve
+        long long ans = -1;
+        if(R < iniRank){
+            vector<long long> a(4);
+            long long __min = 0, __max = 2000000000000000000, mid;
+            while(__min <= __max){
+                mid = (__min + __max) / 2;
+                for(int i = 0; i < 4; i++)a[i] = A[i];
+                for(int i = 0; i < 4; i++){
+                    if(i == X)a[i] += 4 * mid;
+                    else if(i == Y)a[i] -= 2 * mid;
+                    else a[i] -= mid;
+                }
+                //rank
+                up = 0;
+                for(int i = 0; i < 4; i++){
+                    if(i == X)continue;
+                    if(a[X] < a[i])up++;
+                    else if(a[i] < a[X]);
+                    else if(i < X)up++;
+                    else;
+                }
+                up++;
+                if(up == R){
+                    ans = mid;
+                    __max = mid - 1;
+                }else if(up < R){
+                    __max = mid - 1;
+                }else{
+                    __min = mid + 1;
+                }
+            }
+        }else if(R == iniRank)ans = 0;
         cout << ans << endl;
     }
-    //inirank
-    cout << "0" << endl;
-    for(int i = iniRank + 1; i <= 4; i++)cout << "-1" << endl;
     return 0;
 }
