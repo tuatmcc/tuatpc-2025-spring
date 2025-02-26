01a371401ff0c9aacd497cda222ab4fbca648556
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-02-27 03:07:09 +0900

    テスト用の問題を追加

---
+++ 628/tests/validator-konpeitoo.cpp
@@ -0,0 +1,18 @@
+#include "testlib.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MIN_A = 1;
+const int MAX_A = 100;
+const int MIN_B = 1;
+const int MAX_B = 100;
+
+int main(int argc, char* argv[]){
+  registerValidation(argc, argv);
+  inf.readInt(MIN_A, MAX_A, "A");
+  inf.readSpace();
+  inf.readInt(MIN_B, MAX_B, "B");
+  inf.readEoln();
+  inf.readEof();
+  return 0;
+}
