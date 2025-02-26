18da9767dc1cb5b5fe866fa2221b3ead1b3a5234
toufu-24 <120612884+toufu-24@users.noreply.github.com>
2025-02-27 00:48:47 +0900

    制約軽減(仮)

--- amidakuji/tests/constraints.h
+++ amidakuji/tests/constraints.h
@@ -8,14 +8,14 @@ constexpr int NORMAL_MAX_N = 20;
 constexpr int NORMAL_MIN_M = 1;
 constexpr int NORMAL_MAX_M = 100000;
 constexpr int NORMAL_MIN_Q = 1;
-constexpr int NORMAL_MAX_Q = 100000; // 1e5
+constexpr int NORMAL_MAX_Q = 50000; // 5e4
 
 constexpr int HARD_MIN_N = 2;
 constexpr int HARD_MAX_N = 1000000000; // 1e9
 constexpr int HARD_MIN_M = 1;
 constexpr int HARD_MAX_M = 1000000000; // 1e9
 constexpr int HARD_MIN_Q = 1;
-constexpr int HARD_MAX_Q = 100000; // 1e5
+constexpr int HARD_MAX_Q = 50000; // 5e4
 
 constexpr int MIN_N = HARD_MIN_N;
 constexpr int MAX_N = HARD_MAX_N;
