e4b3e61122ac7f2bbd82989ac97f958b9fd37fb4
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-02-28 03:17:14 +0900

    upload_statement_and_testcases のロギング機能を強化

--- scripts/upload_statement_and_testcases.py
+++ scripts/upload_statement_and_testcases.py
@@ -3,9 +3,32 @@ from lib.parser import load_problem, parse_id_in_contest
 from sys import argv
 from pathlib import Path
 from pack_testcases import make_zip
+from typing import Optional, Tuple
 import logging
 
 logger = logging.getLogger(__name__)
+
+def load_setting_files(base_path: Path) -> Optional[Tuple[str, str]]:
+    problem_toml_path = base_path / 'problem.toml'
+    problem_rime_path = base_path / 'PROBLEM'
+
+    if not problem_toml_path.exists():
+        logger.warning(f'problem.toml not found in {base_path}')
+        return None
+    if not problem_rime_path.exists():
+        logger.warning(f'PROBLEM not found in {base_path}')
+        return None
+
+    problem_toml = open(problem_toml_path, 'r').read()
+    problem_rime = open(problem_rime_path, 'r').read()
+
+    return problem_toml, problem_rime
+
+def load_statement(base_path: Path, contest_id: str) -> str:
+    statement_path = base_path / 'ss-out' / f'{contest_id}.md'
+    if not statement_path.exists():
+        logger.warning(f'Statement file not found in: {statement_path}')
+    return open(statement_path, 'r').read()
 
 def upload_statement_and_testcases(username: str, password: str):
     client = Client()
@@ -14,36 +37,29 @@ def upload_statement_and_testcases(username: str, password: str):
     for path in Path('./').glob('*'):
         if not path.is_dir() or not (path / 'PROBLEM').exists():
             continue
+        
+        logger.info(f'Processing for {path.name}')
 
         make_zip(path)
         
-        # 設定ファイルの読み込み
-        problem_toml_path = path / 'problem.toml'
-        problem_rime_path = path / 'PROBLEM'
-
-        if not problem_toml_path.exists():
-            logger.warning(f'problem.toml not found in {path.name}')
-            continue
-        if not problem_rime_path.exists():
-            logger.warning(f'PROBLEM not found in {path.name}')
+        if (ret := load_setting_files(path)) is None:
             continue
 
-        problem_toml = open(problem_toml_path, 'r').read()
-        problem_rime = open(problem_rime_path, 'r').read()
-        
-        # 問題文の読み込み
-        statement_path = path / 'ss-out' / f'{parse_id_in_contest(problem_toml)}.md'
-        if not statement_path.exists():
-            logger.warning(f'Statement file not found: {statement_path}')
+        problem_toml, problem_rime = ret
+        contest_id = parse_id_in_contest(problem_toml)
+        statement = load_statement(path, contest_id)
 
-        statement = open(statement_path, 'r').read()
         problem_id, problem, testcase_sets = load_problem(statement, problem_rime, problem_toml)
 
         client.upload_testcases(problem_id, f'{path.name}.zip')
+        logger.info(f'Uploaded testcases')
 
         client.put_problem(problem_id, problem)
-        
+        logger.info(f'Updated problem statement')
+
         client.initialize_testcase_sets(problem_id)
+        logger.info(f'Deleted all testcase_sets for problem {problem_id}')
+        
         for testcase_set in testcase_sets:
             testcase_ids = client.get_testcase_ids_regex(problem_id, testcase_set.regex)
             if testcase_set.is_sample() or testcase_set.is_all():
@@ -52,15 +68,21 @@ def upload_statement_and_testcases(username: str, password: str):
                 client.post_testcase_set(problem_id, testcase_set)
             testcase_set_id = client.get_testcase_set_id(problem_id, testcase_set.name)
             client.testcases_change_state_multiple(problem_id, testcase_ids, testcase_set_id)
+            logger.info(f'Uploaded testcase_set {testcase_set.name}')
         
         testcases = client.get_testcases(problem_id)[1]
         for explanation_path in (path / 'tests').glob('*.md'):
             md = open(explanation_path, 'r').read()
             testcase_name = explanation_path.stem
-            testcase_id = [testcase.id for testcase in testcases if testcase.name == testcase_name][0]
+            testcase_ids = [testcase.id for testcase in testcases if testcase.name == testcase_name]
+            if len(testcase_ids) == 0:
+                logger.warning(f'Testcase not found for {explanation_path.name}')
+                continue
+            testcase_id = testcase_ids[0]
             testcase = client.get_testcase(problem_id, testcase_id)
             testcase.explanation = md
             client.put_testcase(problem_id, testcase_id, testcase)
+            logger.info(f'Uploaded explanation for {testcase_name}')
 
 if __name__ == '__main__':
     upload_statement_and_testcases(argv[1], argv[2])
