5c33dfbbb38b1fe14582816cb38e183011cc058c
thisagi <106305548+thisagi@users.noreply.github.com>
2025-02-27 12:37:32 +0900

    fix sample file name

--- 628/tests/generator.cpp
+++ 628/tests/generator.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
   gen("00_sample_02.in", 5, 2);
 
   // sample3
-  gen("00_sample_02.in", 5, 20);
+  gen("00_sample_03.in", 5, 20);
 
   for (int t = 0; t < 20; t++) {
     gen(format("01_random_%02d.in", t + 1).c_str(), rnd.next(MIN_A, MAX_A),
