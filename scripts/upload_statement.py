eb9791e440f5776af1f40997723c3f2c4fda7b05
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-02-28 09:17:01 +0900

    問題文のアップロード時に変更されたフィールドを出力するようにした

--- scripts/upload_statement.py
+++ scripts/upload_statement.py
@@ -1,5 +1,4 @@
 from lib.api import Client
-from lib.parser import load_problem, parse_id_in_contest
 from lib.logger import logger
 from lib.util import load_problem_directories
 from lib.problem import Problem
@@ -9,8 +8,25 @@ from upload_sample_explanations import upload_sample_explanations
 from sys import argv
 from pathlib import Path
 
-def upload_statement(client: Client, problem_id: int, problem: Problem):    
+def upload_statement(client: Client, problem_id: int, problem: Problem):
+    old_problem = client.get_problem(problem_id)
     client.put_problem(problem_id, problem)
+    
+    if old_problem.name != problem.name:
+        logger.log(f'問題名が変更されました "{old_problem.name}" -> "{problem.name}"')
+    if old_problem.difficulty != problem.difficulty:
+        logger.log(f'難易度が変更されました "{old_problem.difficulty}" -> "{problem.difficulty}"')
+    if old_problem.statement != problem.statement:
+        logger.log(f'問題文が変更されました "{old_problem.statement}" -> "{problem.statement}"')
+    if old_problem.constraints != problem.constraints:
+        logger.log(f'制約が変更されました "{old_problem.constraints}" -> "{problem.constraints}"')
+    if old_problem.partial_scores != problem.partial_scores:
+        logger.log(f'部分点の説明が変更されました "{old_problem.partial_scores}" -> "{problem.partial_scores}"')
+    if old_problem.input_format != problem.input_format:
+        logger.log(f'入力の説明が変更されました "{old_problem.input_format}" -> "{problem.input_format}"')
+    if old_problem.output_format != problem.output_format:
+        logger.log(f'出力の説明が変更されました "{old_problem.output_format}" -> "{problem.output_format}"')
+
     logger.info(f'Updated problem statement, URL: {client.get_problem_url(problem_id)}')
 
 if __name__ == '__main__':
