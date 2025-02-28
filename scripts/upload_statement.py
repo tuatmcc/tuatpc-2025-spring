ae61330eb803b0427930125690338b7e0242d938
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-02-28 09:30:47 +0900

    問題文のアップロード時にログ出力を改善

--- scripts/upload_statement.py
+++ scripts/upload_statement.py
@@ -11,21 +11,24 @@ from pathlib import Path
 def upload_statement(client: Client, problem_id: int, problem: Problem):
     old_problem = client.get_problem(problem_id)
     client.put_problem(problem_id, problem)
-    
+
+    if old_problem.partial_scores is None:
+        old_problem.partial_scores = ''
+
     if old_problem.name != problem.name:
-        logger.log(f'問題名が変更されました "{old_problem.name}" -> "{problem.name}"')
+        logger.info(f'問題名が変更されました "{old_problem.name}" -> "{problem.name}"')
     if old_problem.difficulty != problem.difficulty:
-        logger.log(f'難易度が変更されました "{old_problem.difficulty}" -> "{problem.difficulty}"')
+        logger.info(f'難易度が変更されました "{old_problem.difficulty}" -> "{problem.difficulty}"')
     if old_problem.statement != problem.statement:
-        logger.log(f'問題文が変更されました "{old_problem.statement}" -> "{problem.statement}"')
+        logger.info(f'問題文が変更されました\n"{old_problem.statement}"\n->\n"{problem.statement}"')
     if old_problem.constraints != problem.constraints:
-        logger.log(f'制約が変更されました "{old_problem.constraints}" -> "{problem.constraints}"')
+        logger.info(f'制約が変更されました\n"{old_problem.constraints}"\n ->\n"{problem.constraints}"')
     if old_problem.partial_scores != problem.partial_scores:
-        logger.log(f'部分点の説明が変更されました "{old_problem.partial_scores}" -> "{problem.partial_scores}"')
+        logger.info(f'部分点の説明が変更されました\n"{old_problem.partial_scores}"\n->\n"{problem.partial_scores}"')
     if old_problem.input_format != problem.input_format:
-        logger.log(f'入力の説明が変更されました "{old_problem.input_format}" -> "{problem.input_format}"')
+        logger.info(f'入力の説明が変更されました\n"{old_problem.input_format}"\n->\n"{problem.input_format}"')
     if old_problem.output_format != problem.output_format:
-        logger.log(f'出力の説明が変更されました "{old_problem.output_format}" -> "{problem.output_format}"')
+        logger.info(f'出力の説明が変更されました\n"{old_problem.output_format}"\n->\n"{problem.output_format}"')
 
     logger.info(f'Updated problem statement, URL: {client.get_problem_url(problem_id)}')
 
