f920aecf4e2b03f2f5db4b3a6ef5e2986a16c50b
Sogo SATO <s216458w@st.go.tuat.ac.jp>
2025-03-02 08:31:46 +0900

    Generatorを修正、WA解を追加

--- TUATPC2024Summer-Heuristic-Admin-ver.-Easy/tests/generator.cc
+++ TUATPC2024Summer-Heuristic-Admin-ver.-Easy/tests/generator.cc
@@ -6,20 +6,54 @@ using namespace std;
 const int N_MIN = 1;
 const int N_MAX = 200000;
 
-const long long Hi_MIN = 1;
-const long long Hi_MAX = 1000000000;
-
 const long long Ai_MIN = 1;
 const long long Ai_MAX = 1000000000;
 
-void gen(const string name, const int n, const pair<vector<long long>, vector<long long>> ah) {
+const long long Bi_MIN = 1;
+const long long Bi_MAX = 1000000000;
+
+
+void gen(const string name, const int n, const pair<vector<long long>, vector<long long>> ab) {
 	ofstream of(name);
 
 	of << n << '\n';
 	for (int i = 0; i < n; ++i)
-		of << ah.first[i] << " \n"[i == n-1];
+		of << ab.first[i] << " \n"[i == n-1];
 	for (int i = 0; i < n; ++i)
-		of << ah.second[i] << " \n"[i == n-1];
+		of << ab.second[i] << " \n"[i == n-1];
+}
+
+pair<vector<long long>, vector<long long>> random_testcases(
+	int random_n, long long single_max, long long _sum_max) {
+	pair<vector<long long>, vector<long long>> ab;
+	long long single_assert = Ai_MIN, sum_assert = 0, sum_max = _sum_max;
+	ab.first = vector<long long>(random_n);
+	ab.second = vector<long long>(random_n);
+	
+	for (int i = 0; i < random_n; ++i) {
+		long long a, b;
+		a = rnd.next(Ai_MIN, max(min(min(sum_max, single_max), sum_max - (random_n - i) * Ai_MIN), Ai_MIN));
+		b = rnd.next(Bi_MIN, max(min(min(sum_max, single_max), sum_max - (random_n - i) * Bi_MIN), Bi_MIN));
+
+		if (i == random_n - 1 && max(a, b) < sum_max) {
+			if (a > b)
+				a = min(sum_max, single_max);
+			else
+				b = min(sum_max, single_max);
+		}
+
+		sum_max -= max(a, b);
+
+		single_assert = max(single_assert, max(a, b));
+		sum_assert += max(a, b);
+
+		ab.first[i] = a;
+		ab.second[i] = b;
+	}
+
+	assert(single_assert <= single_max);
+	assert(sum_assert <= _sum_max);
+	return ab;
 }
 
 
@@ -62,68 +96,49 @@ int main(int argc, char *argv[]) {
 	};
 	gen("00_sample_05.in", 3, s5);
 
-	// Random Ai >= 1600 && Hi >= 4200
+	// Random max(max(Ai, Bi)) < 1600 && sum of max(Ai, Bi) < 4200
 	for (int t = 0; t < 10; ++t) {
-		int random_n = rnd.next(N_MIN, N_MAX);
-		pair<vector<long long>, vector<long long>> random_case(vector<long long>(random_n, 0), vector<long long>(random_n, 0));
+		pair<vector<long long>, vector<long long>> testcase;
+		int random_n = rnd.next(N_MIN, 4199);
 
-		for (int i = 0; i < random_n; ++i) {
-			long long a, h;
-			a = rnd.next(1600LL, Ai_MAX);
-			h = rnd.next(4200LL, Hi_MAX);
-			random_case.first[i] = a;
-			random_case.second[i] = h;
-		}
-
-		gen(format("10_random_large_%02d.in", t).c_str(), random_n, random_case);
+		testcase = random_testcases(random_n, 1599, 4199);
+		gen(format("10_random_%02d.in", t).c_str(), random_n, testcase);
 	}
 
-	// Random Ai <= 1600 && Hi <= 4200
+	// Random max(max(Ai, Bi)) <= 1600 && sum of max(Ai, Bi) < 4200
 	for (int t = 0; t < 10; ++t) {
-		int random_n = rnd.next(N_MIN, N_MAX);
-		pair<vector<long long>, vector<long long>> random_case(vector<long long>(random_n, 0), vector<long long>(random_n, 0));
+		pair<vector<long long>, vector<long long>> testcase;
+		int random_n = rnd.next(N_MIN, 4199);
 
-		for (int i = 0; i < random_n; ++i) {
-			long long a, h;
-			a = rnd.next(Ai_MIN, 1600LL);
-			h = rnd.next(Hi_MIN, 4200LL);
-			random_case.first[i] = a;
-			random_case.second[i] = h;
-		}
-
-		gen(format("11_random_small_%02d.in", t).c_str(), random_n, random_case);
-	}
-	
-	// Random Ai <= 1600 && Hi >= 4200
-	for (int t = 0; t < 10; ++t) {
-		int random_n = rnd.next(N_MIN, N_MAX);
-		pair<vector<long long>, vector<long long>> random_case(vector<long long>(random_n, 0), vector<long long>(random_n, 0));
-
-		for (int i = 0; i < random_n; ++i) {
-			long long a, h;
-			a = rnd.next(Ai_MIN, 1600LL);
-			h = rnd.next(4200LL, Hi_MAX);
-			random_case.first[i] = a;
-			random_case.second[i] = h;
-		}
-
-		gen(format("12_random_mixed_%02d.in", t).c_str(), random_n, random_case);
+		testcase = random_testcases(random_n, 1600, 4199);
+		gen(format("11_random_%02d.in", t).c_str(), random_n, testcase);
 	}
 
-	// Random Ai >= 1600 && Hi <= 4200
+	// Random max(max(Ai, Bi)) < 1600 && sum of max(Ai, Bi) <= 4200
 	for (int t = 0; t < 10; ++t) {
+		pair<vector<long long>, vector<long long>> testcase;
+		int random_n = rnd.next(N_MIN, 4200);
+
+		testcase = random_testcases(random_n, 1599, 4200);
+		gen(format("12_random_%02d.in", t).c_str(), random_n, testcase);
+	}
+
+	// Random max(max(Ai, Bi)) <= 1600 && sum of max(Ai, Bi) <= 4200
+	for (int t = 0; t < 10; ++t) {
+		pair<vector<long long>, vector<long long>> testcase;
+		int random_n = rnd.next(N_MIN, 4200);
+
+		testcase = random_testcases(random_n, 1600, 4200);
+		gen(format("13_random_%02d.in", t).c_str(), random_n, testcase);
+	}
+
+	// All random
+	for (int t = 0; t < 5; ++t) {
+		pair<vector<long long>, vector<long long>> testcase;
 		int random_n = rnd.next(N_MIN, N_MAX);
-		pair<vector<long long>, vector<long long>> random_case(vector<long long>(random_n, 0), vector<long long>(random_n, 0));
 
-		for (int i = 0; i < random_n; ++i) {
-			long long a, h;
-			a = rnd.next(1600LL, Ai_MAX);
-			h = rnd.next(Hi_MIN, 4200LL);
-			random_case.first[i] = a;
-			random_case.second[i] = h;
-		}
-
-		gen(format("13_random_mixed_%02d.in", t).c_str(), random_n, random_case);
+		testcase = random_testcases(random_n, Ai_MAX, LLONG_MAX);
+		gen(format("14_random_%02d.in", t).c_str(), random_n, testcase);
 	}
 
 	// N == 1
@@ -132,22 +147,20 @@ int main(int argc, char *argv[]) {
 	// N == 2
 	gen("20_handmade_02.in", 2, {{2000, 5000}, {2000, 5000}});
 
+	// max(max(Ai, Bi)) == 1600 && sum of max(Ai, Bi) == 4200
+	gen("20_handmade_03.in", 5, {{1600, 300, 300, 300, 300}, {1000, 650, 650, 650, 650}});
+
 	// Min
-	gen("30_min_01.in", 5, {{Ai_MIN, Ai_MIN, Ai_MIN, Ai_MIN, Ai_MIN}, {Hi_MIN, Hi_MIN, Hi_MIN, Hi_MIN, Hi_MIN}});
+	gen("30_min_01.in", 5, {{Ai_MIN, Ai_MIN, Ai_MIN, Ai_MIN, Ai_MIN}, {Bi_MIN, Bi_MIN, Bi_MIN, Bi_MIN, Bi_MIN}});
 
 	// Max
-	gen("30_max_01.in", 5, {{Ai_MAX, Ai_MAX, Ai_MAX, Ai_MAX, Ai_MAX}, {Hi_MAX, Hi_MAX, Hi_MAX, Hi_MAX, Hi_MAX}});
+	gen("30_max_01.in", 5, {{Ai_MAX, Ai_MAX, Ai_MAX, Ai_MAX, Ai_MAX}, {Bi_MAX, Bi_MAX, Bi_MAX, Bi_MAX, Bi_MAX}});
 
 	// N == N_MAX
-	for (int t = 0; t < 10; ++t) {
-		pair<vector<long long>, vector<long long>> n_max_case(vector<long long>(N_MAX, 0), vector<long long>(N_MAX, 0));
-		for (int i = 0; i < N_MAX; ++i) {
-			long long a, h;
-			a = rnd.next(Ai_MIN, Ai_MAX);
-			h = rnd.next(Hi_MIN, Hi_MAX);
-			n_max_case.first[i] = a;
-			n_max_case.second[i] = h;
-		}
+	for (int t = 0; t < 5; ++t) {
+		pair<vector<long long>, vector<long long>> n_max_case;
+
+		n_max_case = random_testcases(N_MAX, Ai_MAX, LLONG_MAX);
 		gen(format("40_max_%02d.in", t).c_str(), N_MAX, n_max_case);
 	}
 	return 0;
